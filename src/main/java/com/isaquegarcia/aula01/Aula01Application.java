81cdc641f51987b7d9fd96e414ee7d5ba4a25b8c
IsaqueGarcia <isaqueemprego1@gmail.com>
2019-06-14 13:11:34 -0300

    Criando operacao de instanciacao

--- src/main/java/com/isaquegarcia/aula01/Aula01Application.java
+++ src/main/java/com/isaquegarcia/aula01/Aula01Application.java
@@ -1,13 +1,33 @@
 package com.isaquegarcia.aula01;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Aula01Application {
+import com.isaquegarcia.aula01.domain.Categoria;
+import com.isaquegarcia.aula01.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class Aula01Application implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Aula01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		repo.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
