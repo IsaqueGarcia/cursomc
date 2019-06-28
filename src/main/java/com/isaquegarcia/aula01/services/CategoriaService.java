415937dadda8501e0f6f8b7537d85e5dd99a620c
IsaqueGarcia <isaqueemprego1@gmail.com>
2019-06-28 17:28:15 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/isaquegarcia/aula01/services/CategoriaService.java
+++ src/main/java/com/isaquegarcia/aula01/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.isaquegarcia.aula01.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,5 +40,9 @@ public class CategoriaService {
 		}catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityViolationException("Não é possivel excluir uma categoria com produto");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
