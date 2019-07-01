9a8896ac3729ac82ff062703b8e738f3420348fb
IsaqueGarcia <isaqueemprego1@gmail.com>
2019-07-01 16:14:29 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/isaquegarcia/aula01/services/CategoriaService.java
+++ src/main/java/com/isaquegarcia/aula01/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.isaquegarcia.aula01.domain.Categoria;
+import com.isaquegarcia.aula01.dto.CategoriaDTO;
 import com.isaquegarcia.aula01.repositories.CategoriaRepository;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -52,5 +53,8 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
